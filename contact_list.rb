0a5b4696471eea6e1028eca0c4b57def0deb6380
tristan <tsmackinlay@gmail.com>
2016-05-30 16:23:30 -0700

    feature 1: display list when no args given

--- contact_list.rb
+++ contact_list.rb
@@ -1,48 +1,16 @@
-require 'csv'
+require_relative 'contact'
 
-# Represents a person in an address book.
-# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
-class Contact
+# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
+class ContactList
 
-  attr_accessor :name, :email
-  
-  # Creates a new contact object
-  # @param name [String] The contact's name
-  # @param email [String] The contact's email address
-  def initialize(name, email)
-    # TODO: Assign parameter values to instance variables.
-  end
+  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
-  # Provides functionality for managing contacts in the csv file.
-  class << self
+end
 
-    # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
-    # @return [Array<Contact>] Array of Contact objects
-    def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
-    end
-
-    # Creates a new contact, adding it to the csv file, returning the new contact.
-    # @param name [String] the new contact's name
-    # @param email [String] the contact's email
-    def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
-    end
-    
-    # Find the Contact in the 'contacts.csv' file with the matching id.
-    # @param id [Integer] the contact id
-    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
-    def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
-    end
-    
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
-    def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
-    end
-
-  end
-
+if ARGV.empty?
+  puts "Here is a list of available commands:
+    new    - Create a new contact
+    list   - List all contacts
+    show   - Show a contact
+    search - Search contacts"
 end
