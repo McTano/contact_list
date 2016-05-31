835fb57550fc42b7d96a76e13c30a02ec20e2b3e
tristan <tsmackinlay@gmail.com>
2016-05-31 13:01:10 -0700

    completed modifications

--- contact.rb
+++ contact.rb
@@ -1,52 +1,50 @@
 require 'csv'
 
-# Represents a person in an address book.
-# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
-  attr_accessor :name, :email
   
-  # Creates a new contact object
-  def initialize(name, email)
-    @name = name
+# Creates a new contact object
+  def initialize(name, email, phone_numbers)
+    @name = name.strip
     @email = email
+    @phone_numbers = phone_numbers
   end
 
   def to_s
-    "#{name} : #{email}"
+    "#{@name} : #{@email}, #{@phone_numbers}"
   end
 
-  # Provides functionality for managing contacts in the csv file.
+  def include?(string)
+    @name.include?(string) || @email.include?(string)
+  end
+
+# Provides functionality for managing contacts in the csv file.
   class << self
 
-    # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
-    def all
+# Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
+    def list
       all_contacts = CSV.read('contacts.csv')
-      all_contacts.map {|arr| Contact.new(arr[0], arr[1])}
+      all_contacts.map {|arr| Contact.new(arr[0], arr[1], arr[2])}
     end
 
-    # Creates a new contact, adding it to the csv file, returning the new contact.
-    def create(name, email)
-    # Instantiates a Contact, adds its data to the 'contacts.csv' file, and returns it.
-      CSV.open('contacts.csv', 'a') { |csv| csv << [name, email] }
-      Contact.new(name, email)
+# Creates a new contact, adding it to the csv file, returning the new contact.
+    def create(name, email, phone_numbers)
+# Instantiates a Contact, adds its data to the 'contacts.csv' file, and returns
+# it.
+      CSV.open('contacts.csv', 'a') { |csv| csv << [name, email, phone_numbers] }
+      Contact.new(name, email, phone_numbers)
     end
     
-    # Finds the Contact in the 'contacts.csv' file with the matching id.
-    # @param id [Integer] the contact id
-    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
-    def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
-      all_contacts = self.all
-      all_contacts[id-1] if id.between (0, all_contacts.length)
+# Find the Contact in the 'contacts.csv' file with the matching id. IDs start
+# at 1, rather than zero, so the index = id-1
+    def show(id)
+      all_contacts = self.list
+      all_contacts[id-1] if id.between?(1, all_contacts.length)
     end
     
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
+# Searches the contacts and returns all matches
     def search(search_string)
-    # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
-      self.all.select {|entry| entry.to_s.downcase.strip.include?(search_string)}
+      self.list.select {|entry| entry.to_s.downcase.strip.include?(search_string)}
     end
 
   end
--- contact_list.rb
+++ contact_list.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require_relative 'contact'
 
 
@@ -6,35 +7,74 @@ class ContactList
     command = ARGV
     case 
     when command == ['list']
-      Contact.all.each_with_index do |entry, i|
-        puts "#{i + 1}. #{entry}"
-      end
-
-    when command == ['new']
-      puts "Enter name: "
-      name = STDIN.gets.chomp
+      id = 1
+      stop = 5
       loop do
-        puts "Enter email"
-        email = STDIN.gets.chomp
-        if !(email =~ /.*@.*/)
-          puts "Invalid email."
-        elsif Contact.all.flatten.any? {|entry| entry.include?(email)}
-          puts "Cannot add duplicate email."
-        else
-        Contact.create(name, email)
-        break
+        until id > stop
+        contacts = Contact.list 
+          puts "#{id}. #{contacts[id - 1  ]}"
+          id += 1
+          exit if id > contacts.length
+        end 
+        stop += 5
+        puts "'y' for next page. 'q' or 'n' to quit."
+        input = STDIN.gets.chomp.strip.downcase
+        case input
+        when 'n', 'q'
+          break
         end
       end
 
+
+    when command == ['new']
+      puts "Enter name: "
+      name = STDIN.gets.chomp.strip
+      
+# loop validates email and checks if it is already present in the csv before
+# creating the new contact. Asks again if email is invalid.
+      email = ''
+      loop do
+        puts "Enter email"
+        email = STDIN.gets.chomp.strip.downcase
+        if !(email =~ /.*@.*/)
+          puts "Invalid email."
+        elsif Contact.list.any? {|entry| entry.include?(email)}
+          puts "Contact is already present."
+          exit
+        else
+          break
+        end
+      end
+# loop adds as many phone numbers as user wants
+      phone_numbers = {}
+      puts "Add a phone number? (y/n)"
+      loop do
+        answer = STDIN.gets.chomp.strip.downcase
+        if answer == 'y'
+          puts "Enter a label. (e.g. 'mobile')"
+          label = STDIN.gets.chomp.strip.downcase.to_sym
+          puts "Enter the phone number."
+          number = STDIN.gets.chomp
+          phone_numbers[label] = number
+          puts "Add another phone number? (y/n)"
+        elsif answer == 'n'
+          break
+        else
+          puts "Please answer 'y' or 'n'."
+        end
+      end
+
+      Contact.create(name, email, phone_numbers)
+
     when command.length == 2 && command[0] == 'show'
       
-      id = command[1].to_i 
-      contact = Contact.find(id)
+      id = command[1].to_i
+      contact = Contact.show(id)
       
       if contact
         puts "#{id}.  #{contact}"
       else
-        puts "No contact matching ID #{id}."
+        puts "No contact matching ID"
       end
 
     when command.length >= 2 && command[0] == 'search'
